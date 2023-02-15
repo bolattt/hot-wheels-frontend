a567425d2783d015e46a234fdba5e3be1d3b606a
Bo Latt <bolattny@gmail.com>
2023-02-15 03:58:24 -0500

    add cars to show collection page

--- src/pages/ShowCollection.jsx
+++ src/pages/ShowCollection.jsx
@@ -5,6 +5,7 @@ import CollectionDetails from "../components/CollectionDetails";
 import SuccessToast from "../components/SuccessToast";
 import ErrorToast from "../components/ErrorToast";
 import ConfirmModal from "../components/ConfirmModal";
+import Car from "../components/Car";
 const API = import.meta.env.VITE_API_URL;
 
 function ShowCollection() {
@@ -14,9 +15,22 @@ function ShowCollection() {
   const { deleteSuccess, deleteError, showPopup, setShowPopup, handleDelete } =
     useDeleteHandle();
 
+  console.log("collection", collection);
+
   return (
-    <section className="mt-30 max-w-7xl mx-auto">
+    <section className="mt-30 mx-auto max-w-7xl pb-12">
       <CollectionDetails collection={collection} setShowPopup={setShowPopup} />
+      <hr />
+      <h2 className="my-6 text-4xl">
+        Cars in this collection :{" "}
+        <span className="">{collection.cars?.length}</span>
+      </h2>
+
+      <div className=" grid grid-cols-fluid gap-16 ">
+        {collection.cars?.map((car) => (
+          <Car car={car} key={car.id} />
+        ))}
+      </div>
       {deleteSuccess && <SuccessToast message="Deleted Successfully!" />}
       {deleteError && <ErrorToast />}
       {showPopup && (
